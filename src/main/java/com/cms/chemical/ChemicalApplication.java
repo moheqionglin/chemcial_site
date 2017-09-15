7199ef64419ecf4021fe4a995b1083e9216aef6f
zhou_wanli <zhou_wanli@wuxiapptec.com>
2017-09-15 23:09:38 +0800

    first commit

---
+++ src/main/java/com/cms/chemical/ChemicalApplication.java
@@ -0,0 +1,12 @@
+package com.cms.chemical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemicalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cms/chemical/ChemicalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cms.chemical;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChemicalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
